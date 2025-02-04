31e612da5218134d1bfeb27efedb6f3f990aab9c
Falk-N <2006870+Falk-N@users.noreply.github.com>
2025-02-04 15:18:22 +0100

    Revert "Update to Version 2"
    This reverts commit 6e54cf81c6ae9913cd04beb9cb10cb003cc439a9.

--- contao/dca/tl_iso_product_collection.php
+++ contao/dca/tl_iso_product_collection.php
@@ -1,19 +1,14 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * This file is part of Inszenium Isotope eCommerce OrderExport.
- * 
- * (c) inszenium 2025 <https://inszenium.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
+/**
+ * IsotopeOrderExport
  *
+ * @copyright  inszenium 2016 <https://inszenium.de>
  * @author     Kirsten Roschanski <kirsten.roschanski@inszenium.de>
- * @package    InszeniumIsotopeOrderExport
+ * @package    IsotopeOrderExport
  * @license    LGPL 
  * @link       https://github.com/inszenium/isotope-export
+ * @filesource
  */
 
 /**
@@ -22,6 +17,64 @@ declare(strict_types=1);
 
 $GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['global_operations']['export'] = array
 (
-  'href' => 'key=export',
-  'class' => 'header_theme_import'
+  'label'         => &$GLOBALS['TL_LANG']['tl_iso_product_collection']['export'],
+  'href'          => '',
+  'class'         => 'header_isotope-export',
+  'attributes'    => 'onclick="Backend.getScrollOffset();" style="display:none"',
+);
+
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['global_operations']['export_information'] = array
+(
+  'label'         => &$GLOBALS['TL_LANG']['tl_iso_product_collection']['export_information'],
+  'class'         => 'header_iso_export_csv isotope-export infobox',
+);
+
+
+
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['global_operations']['export_orders'] = array
+(
+  'label'         => &$GLOBALS['TL_LANG']['tl_iso_product_collection']['export_orders'],
+  'href'          => 'key=export_orders',
+  'class'         => 'header_iso_export_csv isotope-export',
+  'attributes'    => 'onclick="Backend.getScrollOffset();"'
+);
+
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['global_operations']['export_items'] = array
+(
+  'label'         => &$GLOBALS['TL_LANG']['tl_iso_product_collection']['export_items'],
+  'href'          => 'key=export_items',
+  'class'         => 'header_iso_export_csv isotope-export',
+  'attributes'    => 'onclick="Backend.getScrollOffset();"'
+);
+
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['global_operations']['export_bank'] = array
+(
+  'label'         => &$GLOBALS['TL_LANG']['tl_iso_product_collection']['export_bank'],
+  'href'          => 'key=export_bank',
+  'class'         => 'header_iso_export_csv isotope-export',
+  'attributes'    => 'onclick="Backend.getScrollOffset();"'
+);
+
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['global_operations']['export_excel_orders'] = array
+(
+  'label'         => &$GLOBALS['TL_LANG']['tl_iso_product_collection']['export_excel_orders'],
+  'href'          => 'key=export_orders&excel=true',
+  'class'         => 'header_iso_export_csv isotope-export',
+  'attributes'    => 'onclick="Backend.getScrollOffset();"'
+);
+
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['global_operations']['export_excel_items'] = array
+(
+  'label'         => &$GLOBALS['TL_LANG']['tl_iso_product_collection']['export_excel_items'],
+  'href'          => 'key=export_items&excel=true',
+  'class'         => 'header_iso_export_csv isotope-export',
+  'attributes'    => 'onclick="Backend.getScrollOffset();"'
+);
+
+$GLOBALS['TL_DCA']['tl_iso_product_collection']['list']['global_operations']['export_excel_bank'] = array
+(
+  'label'         => &$GLOBALS['TL_LANG']['tl_iso_product_collection']['export_excel_bank'],
+  'href'          => 'key=export_bank&excel=true',
+  'class'         => 'header_iso_export_csv isotope-export',
+  'attributes'    => 'onclick="Backend.getScrollOffset();"'
 );
